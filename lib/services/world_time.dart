eb21e79b7a40153359b6a930b6dd5ccefb924c14
Lduffley <53620669+Lduffley@users.noreply.github.com>
2020-04-22 14:52:52 -0500

    Add files via upload

---
+++ lib/services/world_time.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+  String location; //location name for UI
+  String time; //time in that location
+  String flag; // url to asset flag icon
+  String url; // location url for api endpoint
+
+  WorldTime({this.location, this.flag, this.url});
+
+  Future<void> getTime() async {
+    try {
+      Response response =
+          await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
+
+      // get properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(0, 3);
+//    print(datetime);
+//    print(offset);
+
+      // create DateTime Object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+
+      //set time property
+      time = now.toString();
+    } catch (e) {
+      print('caught error : $e');
+      time = 'could not get time data';
+    }
+  }
+}
